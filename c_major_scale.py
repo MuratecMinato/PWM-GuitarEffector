560af50a7f8aa086059be2ac2599788f56d4d28a
MuratecMinato <yuuichirou.minato@akl.muratec.co.jp>
2023-06-21 11:52:22 +0900

    待ち時間修正

--- c_major_scale.py
+++ c_major_scale.py
@@ -3,34 +3,41 @@ import time
 from machine import PWM
 
 # GP16をPWMに使うための設定
-Led  = PWM(machine.Pin(15, machine.Pin.OUT))
+pwm = PWM(machine.Pin(15, machine.Pin.OUT))
+pwm.duty_u16(52429)
+time.sleep(0.5)
 
-Led.freq(261)	#Cド
-time.sleep(2)
+pwm.freq(261)	#Cド
+time.sleep(0.5)
+print("C")
 
-Led.duty_u16(52429)
-time.sleep(2)
+pwm.freq(293)	#Dレ
+time.sleep(0.5)
+print("D")
 
-Led.freq(293)	#Dレ
-time.sleep(2)
+pwm.freq(329)	#E
+time.sleep(0.5)
+print("E")
 
-Led.freq(329)	#E
-time.sleep(2)
+pwm.freq(349)	#F
+time.sleep(0.5)
+print("F")
 
-Led.freq(349)	#F
-time.sleep(2)
+pwm.freq(392)	#G
+time.sleep(0.5)
+print("G")
 
-Led.freq(392)	#G
-time.sleep(2)
+pwm.freq(440)	#A
+time.sleep(0.5)
+print("A")
 
-Led.freq(440)	#A
-time.sleep(2)
+pwm.freq(494)	#B
+time.sleep(0.5)
+print("B")
 
-Led.freq(494)	#B
-time.sleep(2)
+pwm.freq(523)	#C
+time.sleep(0.5)
+print("C")
 
-Led.freq(523)	#C
-time.sleep(2)
-
-Led.duty_u16(0)
-time.sleep(2)
+pwm.duty_u16(0)
+time.sleep(0.5)
